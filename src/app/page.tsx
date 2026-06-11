47290c9792030466a97c26f49a5847d8f6a8022a
Halyna Bondar <104671539+halynabondar@users.noreply.github.com>
2026-06-11 17:25:49 +0200

    migrate project to TypeScript

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,10 @@
-import styles from "./page.module.css";
 
 export default function Home() {
   return (
     <div>
       <main>
         <div>
-          <h1></h1>
+          <h1>Sport News</h1>
         </div>
       </main>
     </div>
