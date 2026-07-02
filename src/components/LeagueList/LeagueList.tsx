c13ed5aaafd6bf4cef393a3c9685a9b47ebd396c
Halyna Bondar <104671539+halynabondar@users.noreply.github.com>
2026-07-02 15:37:01 +0200

    feat: add responsive sports categories section

--- src/components/LeagueList/LeagueList.tsx
+++ src/components/LeagueList/LeagueList.tsx
@@ -1,16 +1,42 @@
 import styles from "./LeagueList.module.css";
+import Image from "next/image";
+
+const categories = [
+    {title: "Football", image: "/images/Category/Football.png"},
+    {title: ["Basket", "Ball"], image: "/images/Category/Basketball.png"},
+    {title: "Car Sport", image: "/images/Category/Sport-car.png"},
+    {title: ["Table", "Tennis"], image: "/images/Category/Tennis.png"},
+];
 
 export default function LeagueList() {
-  return (
-    <section className={styles.leagueList}>
-      <h2>Popular Leagues</h2>
+    return (
+        <section className={`container ${styles.categorySection}`}>
+            <h2 className={styles.category}>Category</h2>
 
-      <ul>
-        <li>Premier League</li>
-        <li>La Liga</li>
-        <li>Serie A</li>
-        <li>Bundesliga</li>
-      </ul>
-    </section>
-  );
+            <div className={styles.grid}>
+                {categories.map((item, index) => (
+                    <div className={styles.item} key={index}>
+                        <div className={styles.titleCard}>
+                            <h3>
+                                {Array.isArray(item.title)
+                                    ? item.title.map((line, index) => (
+                                        <span key={index}>
+                                        {line}
+                                            {index < item.title.length - 1 && <br/>}
+                                    </span>
+                                    ))
+                                    : item.title}
+                            </h3>
+                        </div>
+
+                        <div className={styles.imageCard}>
+                            <Image
+                                src={item.image}
+                                alt={Array.isArray(item.title) ? item.title.join(" ") : item.title} fill/>
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </section>
+    );
 }
